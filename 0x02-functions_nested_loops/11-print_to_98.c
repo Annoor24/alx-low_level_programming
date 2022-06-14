cb67e1d2019a5e722aab14715e5ca3377673e591
Nuruddin Kabir <kabirnuruddin24@gmail.com>
2022-06-13 17:31:43 -0700

    Task 11 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * print_to_98 - prints all natural numbers from n to 98,
+ * print_to_98 - prints all natural numbers from n to 98
  * followed by a new line
  * @n: print from this number
  */
@@ -26,7 +26,7 @@ void print_to_98(int n)
 			if (j != 98)
 				printf("%d, ", j);
 			else if (j == 98)
-				printf("%d\n"' j);
+				printf("%d\n", j);
 		}
 	}
 }
